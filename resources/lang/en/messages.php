9d01211f83f70f878ff565813cc5b2096f72da12
Gamalsobhy585 <eng.gamalsobhi@gmail.com>
2025-06-08 12:34:54 +0300

    create translate files

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,54 @@
+<?php
+
+return [
+    "hero_section"=>[
+        "title"=>"TAKING QUALITY SERIOUSLY.",
+        "description"=>"At EEC Galva, quality is taken seriously at all levels. EEC Galva has a rigorous quality control program at all steps in the process. From the chemical and zinc compositions to the timing and techniques used to pre-treat and galvanize the product to the finishing works carried out on the product.",
+        "get_in_touch"=>"Get in Touch",
+
+    ],
+    "navbar"=>[
+        "menu_button"=>"MENU",
+        "contact"=>"Contact Us",
+        "search"=>"Search",
+
+    ],
+    "sidebar"=>[
+        "about"=>"About Us",
+        "services"=>"Our Services",
+        "news"=>"News",
+        "careers"=>"Careers",
+        "management"=>"Our Management",
+        "responsibility"=>"Our Responsibility",
+        
+    ],
+    "about_section"=>[
+        "more_about"=>"More About Us",
+
+    ],
+    "info_section"=>[
+        "title"=>"Revolutionize Your Projects",
+        "projects"=>"Projects",
+        "customers"=>"Customers",
+        "productivity"=>"Ton / Month"
+    ],
+    "client_section"=>[
+        "title"=>"Our Clients"
+    ],
+    "footer"=>[
+        "about_us"=>"About Us",
+        "links"=>"Company Links",
+        "service"=>"service",
+        "careers"=>"Careers",
+        "contact"=>"Contact Info",
+        "address"=>"Address",
+        "factory_address"=>"Second Industrial Zone - Block No. 27013 - Obour City, Cairo, Egypt",
+        "phone"=>"Phone",
+        "email"=>"E-mail",
+        "copy_right"=>"EEC GALVA © 2024 Copyrights ."
+        
+    ]
+
+
+
+];
--- resources/views/frontend/includes/footer_below.blade.php
+++ resources/views/frontend/includes/footer_below.blade.php
@@ -2,13 +2,11 @@
 
     <div class="bottom-bar-content">
         <div id="copyright">EEC GALVA © 2024 Copyrights .
-        </div><!-- /#copyright -->
     </div><!-- /.bottom-bar-content -->
 
     <div class="bottom-bar-menu">
         <ul class="bottom-nav">
-{{--            <li><a href="{{ route('privacyStatementFront') }}">Privacy Statement</a></li>--}}
-{{--            <li><a href="{{ route('websiteDisclaimerFront') }}">Website Disclaimer</a></li>--}}
+
         </ul>
     </div><!-- /.bottom-bar-menu -->
 </div>
